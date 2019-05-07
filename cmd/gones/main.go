408da83d66b4dc72653c4a48f9c14a8fd5733d07
Yuxiang Zhu <vfreex@gmail.com>
2019-05-07 16:58:39 +0800

    list of passed tests

--- cmd/gones/main.go
+++ cmd/gones/main.go
@@ -13,20 +13,26 @@ var logger = logger2.GetLogger()
 func main() {
 	var fileName string
 
-	fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/cpu_reset/ram_after_reset.nes" // passed
-	fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/cpu_reset/registers.nes" // passed
-
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/cpu_reset/ram_after_reset.nes" // passed
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/cpu_reset/registers.nes" // passed
+	//
 	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/instr_misc/instr_misc.nes"
+	//
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_nes_cpu_test5/official.nes" // passed
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_nes_cpu_test5/cpu.nes" // illegal opcode
+	//
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_ppu_tests_2005.09.15b/palette_ram.nes" // passed
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_ppu_tests_2005.09.15b/power_up_palette.nes" //failed
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_ppu_tests_2005.09.15b/sprite_ram.nes" // passed
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_ppu_tests_2005.09.15b/vbl_clear_time.nes" // failed
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_ppu_tests_2005.09.15b/vram_access.nes" // passed
 
-	fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_nes_cpu_test5/official.nes" // passed
-	// fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_nes_cpu_test5/cpu.nes" // illegal opcode
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/cpu_exec_space/test_cpu_exec_space_ppuio.nes" // failed
+	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/cpu_interrupts_v2/cpu_interrupts.nes" // failed
 
-	fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_ppu_tests_2005.09.15b/palette_ram.nes" // passed
-	//fileName = "/Users/vfreex/Documents/hack/NES/tests/nes-test-roms/blargg_ppu_tests_2005.09.15b/power_up_palette.nes"
+	//fileName = "/Users/vfreex/Documents/hack/NES/color_test_src/color_test.nes" // passed
 
-	//fileName = "/Users/vfreex/Downloads/nestests/blargg_ppu_tests_2005.09.15b/sprite_ram.nes" // passed
-	//fileName = "/Users/vfreex/Downloads/nestests/blargg_ppu_tests_2005.09.15b/palette_ram.nes" // passed
-	//fileName = "/Users/vfreex/Downloads/nestests/blargg_ppu_tests_2005.09.15b/vram_access.nes" // failed
+
 
 	flag.Parse()
 	if flag.NArg() > 1 {
